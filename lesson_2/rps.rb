5b260422e8d65e524d5232f8f1d7dc3a87c97f63
Alex Bair <alexbair143@gmail.com>
2022-11-24 04:32:52 +0000

    Added various different computer opponents and unique gameplay strategies. Will work on further bonus features in later update. Began fiddling with how to keep track and interact with the move history, but needs fleshed out more and actually displayed.

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,12 +1,21 @@
 =begin
 To do:
 - clear the screen after each round
+- keep track of the round number
 - standardize display
+  - better interface
+
 - organize RPSGame#play method into different phases
   - opening phase
   - gameplay phase
   - post-game phase
+- allow user and opponent to make opening statements prior to beginning main game loop
+- have computer taunt after 8 rounds
+- have computer make a statement after winning and after losing
 - check flow of game & determine when to have sleeps and how long
+- implement move history (see exploration_space.rb for initial thoughts and draft ideas)
+  - allow user to check move history by typing 'history' before choosing a move
+- rubocop program
 
 Ask LS Slack question about how Ruby looks up constants for child and parent classes.
 See exploration_space.rb and 
@@ -19,7 +28,7 @@ for examples of what I'm talking about.
 
 class Score
   WINNING_SCORE = 10
-  STARTING_SCORE = 9
+  STARTING_SCORE = 0
 
   def initialize
     @score = STARTING_SCORE
@@ -81,6 +90,26 @@ class Spock < Move
   LOSES_AGAINST = ['lizard', 'paper']
 end
 
+class MoveHistory
+  attr_accessor :history
+  
+  def initialize
+    @history = []
+  end
+  
+  def reset
+    @history = []
+  end
+  
+  def record(move)
+    history << move
+  end
+  
+  def to_s
+    history
+  end
+end
+
 class Player
   USER_INPUT_CONVERSION = {
     'rock' => Rock.new,
@@ -90,11 +119,16 @@ class Player
     'spock' => Spock.new
   }
   
-  attr_accessor :move, :name, :score
+  attr_accessor :move, :name, :score, :move_history
 
   def initialize
     set_name
     @score = Score.new
+    @move_history = MoveHistory.new
+  end
+  
+  def record_move
+    move_history.record(move.to_s)
   end
   
   def to_s
@@ -125,26 +159,156 @@ class Human < Player
     end
 
     self.move = USER_INPUT_CONVERSION[choice]
+    self.record_move
   end
 end
 
 class Computer < Player
   def set_name
-    self.name = ['Robot1', 'Robot2', 'Robot3'].sample
+    @name = self.class.to_s
   end
 
-  def choose
+  def choose_random
     self.move = USER_INPUT_CONVERSION[(USER_INPUT_CONVERSION.keys.sample)]
+    self.record_move
+  end
+  
+  def choose_rock
+    self.move = Rock.new
+    self.record_move
+  end
+  
+  def choose_paper
+    self.move = Paper.new
+    self.record_move
+  end
+  
+  def choose_scissors
+    self.move = Scissors.new
+    self.record_move
+  end
+  
+  def choose_lizard
+    self.move = Lizard.new
+    self.record_move
+  end
+  
+  def choose_spock
+    self.move = Spock.new
+    self.record_move
+  end
+  
+  def choose_losing_move(human_move)
+    self.move = human_move.class::WINS_AGAINST[0]
+    self.record_move
+  end
+  
+  def choose_winning_move(human_move)
+    self.move = human_move.class::LOSES_AGAINST[0]
+    self.record_move
+  end
+end
+
+class Rockman < Computer
+  def choose(human_move, human_score)
+    choose_rock
+  end
+end
+
+class Papyrus < Computer
+  def choose(human_move, human_score)
+    choose_paper
+  end
+end
+
+class DJCutman < Computer
+  def set_name
+    @name = 'DJ Cutman'
+  end
+  
+  def choose(human_move, human_score)
+    choose_scissors
+  end
+end
+
+class Martin < Computer
+  def choose(human_move, human_score)
+    choose_lizard
+  end
+end
+
+class Picard < Computer
+  def choose(human_move, human_score)
+    choose_spock
+  end
+end
+
+class GlassJoe < Computer
+  def set_name
+    @name = 'Glass Joe'
+  end
+  
+  def choose(human_move, human_score)
+    choose_losing_move(human_move)
+  end
+end
+
+class Glados < Computer
+  def set_name
+    @name = 'GLaDOS'
+  end
+  
+  def choose(human_move, human_score)
+    choose_winning_move(human_move)
+  end
+end
+
+class Zenos < Computer
+  def choose(human_move, human_score)
+    case human_score <=> self.score
+    when 0 then choose_random
+    when -1 then choose_losing_move(human_move)
+    when 1 then choose_winning_move(human_move)
+    end
   end
 end
 
 # Game Orchestration Engine
 class RPSGame
+  OPPONENTS = {
+    'rockman' => Rockman.new,
+    'papyrus' => Papyrus.new,
+    'dj cutman' => DJCutman.new,
+    'martin' => Martin.new,
+    'picard' => Picard.new,
+    'glass joe' => GlassJoe.new,
+    'glados' => Glados.new,
+    'zenos' => Zenos.new
+  }
+
   attr_accessor :human, :computer
 
   def initialize
     @human = Human.new
-    @computer = Computer.new
+    choose_opponent
+  end
+
+  def choose_opponent
+    input = ''
+    loop do
+      puts "Please choose your opponent:"
+      list_opponents
+      input = gets.chomp.downcase
+      break if OPPONENTS.has_key?(input)
+      puts "Invalid opponent. Please type in a valid opponent."
+    end
+    @computer = OPPONENTS[input] #where would be best to put the hash to convert the string input into the actual opponent object?
+  end
+
+  def list_opponents
+    ['Rockman', 'Papyrus', 'DJ Cutman', 'Martin', 'Picard', 'Glass Joe', 'GLaDOS', 'Zenos'].each do |name|
+      puts "- #{name}"
+    end
   end
 
   def display_welcome_message
@@ -227,7 +391,7 @@ class RPSGame
       clear_screen
       display_score
       human.choose
-      computer.choose
+      computer.choose(human.move, human.score)
       display_moves
       update_score
       display_round_winner
